1982c8e5b09377de75a21858c045e7f354d91987
wgdcwd <honghcy01@gmail.com>
2025-11-28 16:04:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ from pipeline.train_xgb import train_and_evaluate_xgb_for_chunk
 
 
 def run_pipeline(chunk_size_mb: int = 1) -> None:
+    #extract features 실행부분
 
+    #----------
+
+    #모델 트레이닝
     train_and_evaluate_xgb_for_chunk(chunk_size_mb)
+    #----------
 
+    #벤치마크 실행부분
+
+    #----------
 
 
 if __name__ == "__main__":
